3de6a0d7af735d76ef269c915909adc822c22fbc
dzekiczan1 <p.rzadkowolski@gmail.com>
2025-01-01 19:37:48 +0100

    animate donuts

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,7 +1,8 @@
 import {OrbitControls, Text3D, Center, useMatcapTexture} from '@react-three/drei';
 import {Perf} from 'r3f-perf';
-import {useState, useEffect} from "react";
+import {useState, useEffect, useRef} from "react";
 import * as THREE from 'three';
+import {useFrame} from "@react-three/fiber";
 
 const torusGeometry = new THREE.TorusGeometry(1, 0.4, 16, 32);
 const material = new THREE.MeshMatcapMaterial();
@@ -10,6 +11,15 @@ export default function Experience() {
     const [matcapTexture] = useMatcapTexture('7B5254_E9DCC7_B19986_C8AC91', 256);
     // const [torusGeometry, setTorusGeometry] = useState();
     // const [material, setMaterial] = useState();
+    // const donutsGroup = useRef();
+
+    const donuts = useRef([]);
+
+    useFrame((state, delta) => {
+        for (const donut of donuts.current) {
+            donut.rotation.y += delta * 0.2;
+        }
+    });
 
     useEffect(() => {
         matcapTexture.colorSpace = THREE.SRGBColorSpace
@@ -40,23 +50,24 @@ export default function Experience() {
                 HELLO R3F
             </Text3D>
         </Center>
-        {[...Array(100)].map((value, index) =>
-            <mesh
-                key={index}
-                geometry={torusGeometry}
-                material={material}
-                position={[
-                    (Math.random() - 0.5) * 10,
-                    (Math.random() - 0.5) * 10,
-                    (Math.random() - 0.5) * 10
-                ]}
-                scale={0.2 + Math.random() * 0.2}
-                rotation={[
-                    Math.random() * Math.PI,
-                    Math.random() * Math.PI,
-                    0
-                ]}
-            />
-        )}
+            {[...Array(100)].map((value, index) =>
+                <mesh
+                    ref={(element) => donuts.current[index] = element}
+                    key={index}
+                    geometry={torusGeometry}
+                    material={material}
+                    position={[
+                        (Math.random() - 0.5) * 10,
+                        (Math.random() - 0.5) * 10,
+                        (Math.random() - 0.5) * 10
+                    ]}
+                    scale={0.2 + Math.random() * 0.2}
+                    rotation={[
+                        Math.random() * Math.PI,
+                        Math.random() * Math.PI,
+                        0
+                    ]}
+                />
+            )}
     </>
 }
