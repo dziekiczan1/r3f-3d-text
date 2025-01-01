7e817359b701698c3d280b7bec124011f2c500c6
dzekiczan1 <p.rzadkowolski@gmail.com>
2025-01-01 19:04:37 +0100

    add donuts

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -20,8 +20,27 @@ export default function Experience() {
                 bevelSegments={5}
             >
                 HELLO R3F
-                <meshMatcapMaterial matcap={matcapTexture} />
+                <meshMatcapMaterial matcap={matcapTexture}/>
             </Text3D>
         </Center>
+        {[...Array(100)].map((value, index) =>
+            <mesh
+                key={index}
+                position={[
+                    (Math.random() - 0.5) * 10,
+                    (Math.random() - 0.5) * 10,
+                    (Math.random() - 0.5) * 10
+                ]}
+                scale={0.2 + Math.random() * 0.2}
+                rotation={[
+                    Math.random() * Math.PI,
+                    Math.random() * Math.PI,
+                    0
+                ]}
+            >
+                <torusGeometry/>
+                <meshMatcapMaterial matcap={matcapTexture}/>
+            </mesh>
+        )}
     </>
 }
