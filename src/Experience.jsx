8b19445026393f3b2986f5610997cfea0e401cb1
dzekiczan1 <p.rzadkowolski@gmail.com>
2025-01-01 19:27:22 +0100

    optimize geometries and material

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,14 +1,32 @@
 import {OrbitControls, Text3D, Center, useMatcapTexture} from '@react-three/drei';
 import {Perf} from 'r3f-perf';
+import {useState, useEffect} from "react";
+import * as THREE from 'three';
+
+const torusGeometry = new THREE.TorusGeometry(1, 0.4, 16, 32);
+const material = new THREE.MeshMatcapMaterial();
 
 export default function Experience() {
     const [matcapTexture] = useMatcapTexture('7B5254_E9DCC7_B19986_C8AC91', 256);
+    // const [torusGeometry, setTorusGeometry] = useState();
+    // const [material, setMaterial] = useState();
+
+    useEffect(() => {
+        matcapTexture.colorSpace = THREE.SRGBColorSpace
+        matcapTexture.needsUpdate = true
+
+        material.matcap = matcapTexture;
+        material.needsUpdate = true;
+    }, []);
 
     return <>
         <Perf position="top-left"/>
         <OrbitControls makeDefault/>
+        {/*<torusGeometry ref={setTorusGeometry}/>*/}
+        {/*<meshMatcapMaterial ref={setMaterial} matcap={matcapTexture}/>*/}
         <Center>
             <Text3D
+                material={material}
                 font="./fonts/helvetiker_regular.typeface.json"
                 size={0.75}
                 height={0.2}
@@ -20,12 +38,13 @@ export default function Experience() {
                 bevelSegments={5}
             >
                 HELLO R3F
-                <meshMatcapMaterial matcap={matcapTexture}/>
             </Text3D>
         </Center>
         {[...Array(100)].map((value, index) =>
             <mesh
                 key={index}
+                geometry={torusGeometry}
+                material={material}
                 position={[
                     (Math.random() - 0.5) * 10,
                     (Math.random() - 0.5) * 10,
@@ -37,10 +56,7 @@ export default function Experience() {
                     Math.random() * Math.PI,
                     0
                 ]}
-            >
-                <torusGeometry/>
-                <meshMatcapMaterial matcap={matcapTexture}/>
-            </mesh>
+            />
         )}
     </>
 }
